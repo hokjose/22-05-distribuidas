95038e2667693d9c675ac3de6524f042a54ac28c
hokjose <138739527+hokjose@users.noreply.github.com>
2025-07-02 00:31:09 -0500

    Add files via upload

---
+++ ms-factura/src/main/java/com/example/ms_factura/MsFacturaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_factura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFacturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFacturaApplication.class, args);
+	}
+
+}
---
+++ ms-factura/src/test/java/com/example/ms_factura/MsFacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms_factura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFacturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
